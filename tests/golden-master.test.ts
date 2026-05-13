dfbcaa87d1b4637d6928204d76feb99401df6df7
hezermozo <h.jrencontre@gmail.com>
2026-05-13 10:46:41 +0200

    refactor: cloned legacy into src to run golden test

--- tests/golden-master.test.ts
+++ tests/golden-master.test.ts
@@ -1,16 +1,30 @@
 import * as fs from "fs";
 import * as path from "path";
-import { run } from "../legacy/orderReportLegacy";
+import { run } from "../src/orderReportLegacy";
 
 describe("Golden Master Regression Test", () => {
-  it("refactored output should match legacy output", () => {
-    const legacyReport = fs.readFileSync(
-      path.join(__dirname, "../legacy/expected/report.txt"),
-      "utf-8",
-    );
+  it("refactored output text should exactly match legacy output text", () => {
+    const legacyReport = fs
+      .readFileSync(
+        path.join(__dirname, "../legacy/expected/report.txt"),
+        "utf-8",
+      )
+      .replace(/\r?\n$/, ""); // removing trailing line : report.txt may have trailing line, run() string won't.
 
     const refactoredReport = run();
 
-    expect(refactoredReport).toBe(legacyReport);
+    expect(refactoredReport).toEqual(legacyReport);
+  });
+
+  it("refactored output json should exactly match legacy output json", () => {
+    const refactoredJson = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "../src/output.json"), "utf-8"),
+    );
+
+    const legacyJson = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "../legacy/output.json"), "utf-8"),
+    );
+
+    expect(refactoredJson).toEqual(legacyJson);
   });
 });
